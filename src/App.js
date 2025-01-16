e1190da5b69a2c8034a9503df0ee243b4703630b
DevDinith <dinith@thestudentsvisa.com>
2025-01-16 23:45:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
-
+import AboutPage from "./pages/AboutPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
