d274e1537a7565f1dabf276942722c4406de05e3
dilanjanranmalinda <dilanjanranmalinda@gmail.com>
2025-02-22 23:54:15 +0530

    added reach-us page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutPage from "./pages/about/AboutPage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavbarDefault from "./templates/Navbar";
 import Footer from "./templates/Footer";
+import ReachUs from "./pages/reach-us/ReachUs";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/know-us" element={<AboutPage />} />
+          <Route path="/reach-us" element={<ReachUs />} />
         </Routes>
         <Footer />
       </Router>
